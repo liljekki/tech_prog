92b41d3190f0f4ae74aa6dc008aae0e16faf8ba4
liljekki <73700767+liljekki@users.noreply.github.com>
2023-11-12 13:25:30 +0200

    lab

--- lab2_test.py
+++ lab2_test.py
@@ -36,7 +36,14 @@ class TestNotebook(unittest.TestCase):
             self.notebook.add_note("", "Вміст нотатки 1")
 
 if __name__ == '__main__':
-    from xmlrunner import XMLTestRunner
+    try:
+    # 👇️ using Python 3.10+
+        from xmlrunner import XMLTestRunner
+    except ImportError:
+    # 👇️ using Python 3.10-
+        from collections.abc import MutableMapping
+        from collections import MutableMapping
+    
     runner = XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     unittest.main()
